96719f5c686252050d2f91d51425b6b5d7d477ea
rajat7630 <rajatgarg7630@gmail.com>
2020-08-02 18:23:41 +0530

    fix: minor bugs

--- Server/migrations/20200616122626_turn_integer.js
+++ Server/migrations/20200616122626_turn_integer.js
@@ -1,11 +1,7 @@
 
 exports.up = async function (knex) {
 
-    await knex.schema.table("problems", (table) => {
-        table.string("description", 10000);
-        table.integer('timelimit');
-        table.integer('datalimit');
-    })
+ 
 };
 
 exports.down = function (knex) {
